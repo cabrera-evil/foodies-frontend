d4ab1bd45abe21a599be1c265a01d3ba5283660b
cabrera-evil <douglascabrera.dev@gmail.com>
2024-03-02 17:21:01 -0600

    feat: meals page components

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,9 +1,27 @@
+import MelasGrid from '../_components/meals/meals-grid';
+import styles from './page.module.css'
+import Link from 'next/link'
+
 export default function MealsPage() {
     return (
-        <main>
-            <h1 style={{ color: 'white', textAlign: 'center' }}>
-                Meals
-            </h1>
-        </main>
+        <>
+            <header className={styles.header}>
+                <h1>
+                    Delicious Meals, created{' '}
+                    <span className={styles.highlight}>by you</span>
+                </h1>
+                <p>
+                    Choose your favorite meal and create it by yourself!
+                </p>
+                <p className={styles.cta}>
+                    <Link href="/meals/share">
+                        Share your favorite recipe!
+                    </Link>
+                </p>
+            </header>
+            <main className={styles.main}>
+                <MelasGrid meals={[]}/>
+            </main>
+        </>
     );
 }
