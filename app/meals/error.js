e10146693b2dd45daf0a7a6faa5e45f4738dedbc
cabrera-evil <douglascabrera.dev@gmail.com>
2024-03-02 18:50:06 -0600

    feat: meals error handling

---
+++ app/meals/error.js
@@ -0,0 +1,10 @@
+'use client'
+
+export default function Error() {
+    return (
+        <main className="error">
+            <h1>Oops! Something went wrong</h1>
+            <p>Sorry, we are unable to load the meals at the moment. Please try again later.</p>
+        </main>
+    )
+}
