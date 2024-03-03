bcac5894b08179d82e1e8244023ed5d2050086b5
cabrera-evil <douglascabrera.dev@gmail.com>
2024-03-02 18:52:18 -0600

    feat: custom not found page

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <main className="not-found">
+            <h1>404 - Not Found</h1>
+            <p>Sorry, we are unable to find the page you are looking for.</p>
+        </main>
+    )
+}
