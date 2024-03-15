e66b2dc691cca7df0c94a1aad2004c2dde2a0257
cabrera-evil <douglascabrera.dev@gmail.com>
2024-03-14 19:01:06 -0600

    feat: upload meal action

--- app/_lib/meals.js
+++ app/_lib/meals.js
@@ -1,4 +1,7 @@
+import fs from 'fs'
 import sql from 'better-sqlite3'
+import slugify from 'slugify'
+import xss from 'xss'
 
 const db = sql('meals.db')
 
@@ -8,4 +11,18 @@ export async function findAll() {
 
 export async function findBySlug(slug) {
     return await db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug)
+}
+
+export async function saveMeal(meal) {
+    meal.slug = slugify(meal.title, { lower: true });
+    meal.instructions = xss(meal.instructions);
+    const extension = meal.image.name.split('.').pop();
+    const fileName = `${meal.slug}.${extension}`;
+    const stream = fs.createWriteStream(`public/images/${fileName}`);
+    const buffer = await meal.image.arrayBuffer();
+    stream.write(Buffer.from(buffer), (err) => {
+        if (err) throw err;
+    });
+    meal.image = `/images/${fileName}`;
+    db.prepare('INSERT INTO meals (title, summary, instructions, image, creator, creator_email, slug) VALUES (?, ?, ?, ?, ?, ?, ?)').run(meal.title, meal.summary, meal.instructions, meal.image, meal.creator, meal.creator_email, meal.slug);
 }
--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,5 +1,6 @@
 import ImagePicker from '@/app/_components/meals/image-picker';
 import styles from './page.module.css';
+import { shareMeal } from '@/app/_lib/actions';
 
 export default function ShareMealPage() {
     return (
@@ -11,7 +12,7 @@ export default function ShareMealPage() {
                 <p>Or any other meal you feel needs sharing!</p>
             </header>
             <main className={styles.main}>
-                <form className={styles.form}>
+                <form className={styles.form} action={shareMeal}>
                     <div className={styles.row}>
                         <p>
                             <label htmlFor="name">Your name</label>
